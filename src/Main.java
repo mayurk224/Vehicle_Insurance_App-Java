bc18eda0c2e1654eee726873ce86a25dac6fad5e
Mayur Kamble <mayurkamble0250@gmail.com>
2024-10-23 08:52:29 +0530

    Added CustomerPanel to Main Screen

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,56 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import javax.swing.*;
+import javax.swing.border.TitledBorder;
+import java.awt.*;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+public class Main extends JFrame {
+    Font myFont = new Font("SansSerif", Font.BOLD, 20);
+    Color myColor = Color.GRAY;
+
+    // Panel 1
+    JTextField subFName,subLName,subCity,subPhone;
+
+    // Constructor
+    public Main(){
+        CustomizePanel1();
+    }
+
+    private void CustomizePanel1(){
+        JPanel p1 = new JPanel();
+        TitledBorder titledBorder = BorderFactory.createTitledBorder
+                (BorderFactory.createLineBorder(Color.gray,1),
+                        "Customer", TitledBorder.CENTER,TitledBorder.DEFAULT_POSITION, myFont,myColor);
+
+        p1.setBorder(titledBorder);
+
+        JLabel fNameLBL = new JLabel("First Name");
+        JLabel lNameLBL = new JLabel("Last Name");
+        JLabel CityLBL = new JLabel("City");
+        JLabel phoneLBL = new JLabel("Phone");
+
+        subFName = new JTextField();    subFName.setOpaque(false);
+        subLName = new JTextField();    subLName.setOpaque(false);
+        subCity = new JTextField();     subCity.setOpaque(false);
+        subPhone = new JTextField();    subPhone.setOpaque(false);
+
+        p1.add(fNameLBL);   p1.add(subFName);
+        p1.add(lNameLBL);   p1.add(subLName);
+        p1.add(CityLBL);    p1.add(subCity);
+        p1.add(phoneLBL);   p1.add(subPhone);
+
+        p1.setBounds(15,15,300,300);
+        p1.setLayout(new GridLayout(4,2));
+
+        // Adding panel to jframe
+        setLayout(null);
+        add(p1);
+
+    }
+
+    public static void main(String[] args) {
+        Main main = new Main();
+        main.setVisible(true);
+        main.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        main.setTitle("Insurance Company System");
+        main.setBounds(0,0,1920,1080);
     }
 }
