24f6dae81a89c0a3ab903793f74cc2d0cf5858ab
Mayur Kamble <mayurkamble0250@gmail.com>
2024-10-23 09:55:17 +0530

    Added VehiclePanel to Main Screen

--- src/Main.java
+++ src/Main.java
@@ -9,9 +9,15 @@ public class Main extends JFrame {
     // Panel 1
     JTextField subFName,subLName,subCity,subPhone;
 
+    // Panel 2
+    JTextField model,manufacture,plateNb,estimated;
+    JRadioButton damageRadio1, damageRadio2, damageRadio3,damageRadio4;
+    ButtonGroup G1;
+
     // Constructor
     public Main(){
         CustomizePanel1();
+        CustomizePanel2();
     }
 
     private void CustomizePanel1(){
@@ -37,14 +43,68 @@ public class Main extends JFrame {
         p1.add(CityLBL);    p1.add(subCity);
         p1.add(phoneLBL);   p1.add(subPhone);
 
-        p1.setBounds(15,15,300,300);
-        p1.setLayout(new GridLayout(4,2));
+        p1.setBounds(15,15,300,180);
+        p1.setLayout(new GridLayout(4,1));
 
         // Adding panel to jframe
         setLayout(null);
         add(p1);
-
     }
+
+
+    private void CustomizePanel2(){
+        JPanel p2 = new JPanel();
+        TitledBorder titledBorder = BorderFactory.createTitledBorder
+                (BorderFactory.createLineBorder(Color.gray,1),
+                        "Vehicle", TitledBorder.CENTER,TitledBorder.DEFAULT_POSITION, myFont,myColor);
+
+        p2.setBorder(titledBorder);
+
+        // Jlabel
+        JLabel plateNbLBL = new JLabel("Plate Nb");
+        JLabel modelLBL = new JLabel("Model Year");
+        JLabel manufactureLBL = new JLabel("Manufacture");
+        JLabel estimatedLBL = new JLabel("Estimated Value");
+        JLabel damageLBL = new JLabel("Major Damage");
+
+        // Jtextfields
+        plateNb = new JTextField();      plateNb.setOpaque(false);
+        model = new JTextField();        model.setOpaque(false);
+        manufacture = new JTextField();  manufacture.setOpaque(false);
+        estimated = new JTextField();    estimated.setOpaque(false);
+
+        // Jradiobutton
+        damageRadio1 = new JRadioButton();  damageRadio1.setText("Motor");
+        damageRadio2 = new JRadioButton();  damageRadio2.setText("Wheel");
+        damageRadio3 = new JRadioButton();  damageRadio3.setText("Body");
+        damageRadio4 = new JRadioButton();  damageRadio4.setText("None");
+
+        G1 = new ButtonGroup();
+        G1.add(damageRadio1);
+        G1.add(damageRadio2);
+        G1.add(damageRadio3);
+        G1.add(damageRadio4);
+
+        // Adding component to p2
+        p2.add(plateNbLBL);       p2.add(plateNb);
+        p2.add(modelLBL);         p2.add(model);
+        p2.add(manufactureLBL);   p2.add(manufacture);
+        p2.add(estimatedLBL);     p2.add(estimated);
+        p2.add(damageLBL);
+        p2.add(damageRadio1);
+        p2.add(damageRadio2);
+        p2.add(damageRadio3);
+        p2.add(damageRadio4);
+
+        // customize p2
+        p2.setBounds(15,210,300,400);
+        p2.setLayout(new GridLayout(13,1));
+        add(p2);
+    }
+
+
+
+
 
     public static void main(String[] args) {
         Main main = new Main();
